68dc94fefb63243c7825bd338117a61447cd2bab
salarGholami <salargholami021@gmail.com>
2025-04-27 08:21:15 +0330

    Ts inference state type

--- src/components/state/NotList.tsx
+++ src/components/state/NotList.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 
 const NoteList: React.FC = () => {
-  const [notes, setNotes] = useState("");
+  const [note, setNote] = useState("");
 
-  const handleAddNote = () => {};
+  const [notes, setNotes] = useState<string[]>([]);
+
+  const handleAddNote = () => {
+    setNote("");
+    setNotes((prevNotes) => [...prevNotes, note]);
+  };
 
   return (
     <div>
       <h1>NoteList</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note}>{note}</li>
+        ))}
+      </ul>
       <input
         type="text"
-        value={notes}
-        onChange={(e) => setNotes(e.target.value)}
+        value={note}
+        onChange={(e) => setNote(e.target.value)}
       />
       <button onClick={handleAddNote}>add</button>
     </div>
